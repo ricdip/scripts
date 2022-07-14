cdd0ff3a39198a98c7dafb0ef8eefaa702fca6a9
ricdip <riccardodpr@gmail.com>
2022-07-14 12:50:01 +0200

    Arch

---
+++ Linux/touchpad.sh
@@ -0,0 +1,31 @@
+#/bin/sh
+
+function help()
+{
+    cat << HELP_MSG
+    
+    Usage: $(basename $0) <option>
+
+    This script allows to control the touchpad
+
+    Options:
+        --help | -h         Display this usage message 
+        --disable | -d      Disable touchpad
+        --enable | -e       Enable touchpad
+
+HELP_MSG
+
+}
+
+
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    help;
+
+elif [[ "$1" == "--disable" || "$1" == "-d" ]]; then
+    xinput disable "ETPS/2 Elantech Touchpad"
+    notify-send "Touchpad" "Touchpad disabled"
+
+elif [[ "$1" == "--enable" || "$1" == "-e" ]]; then
+    xinput enable "ETPS/2 Elantech Touchpad"
+    notify-send "Touchpad" "Touchpad enabled"
+fi
