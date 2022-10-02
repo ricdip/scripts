f13182ea358a4be442d37b1be58b5210a88dac4a
ricdip <riccardodpr@gmail.com>
2022-10-03 00:26:48 +0200

    Arch

--- Linux/vim-plug_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# installing vim-plug in nvim
-mkdir -p $HOME/.config/nvim/autoload
-mkdir -p $HOME/.config/nvim/plugged
-cd $HOME/.config/nvim/autoload
-
-# downloading vim-plug
-wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
