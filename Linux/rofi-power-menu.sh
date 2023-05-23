2ceb78bed6bc0c32c2a73b8bdbbbf192a8ef2dac
ricdip <riccardodpr@gmail.com>
2023-05-24 00:45:06 +0200

    Arch: rofi-power-menu

---
+++ Linux/rofi-power-menu.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+POWEROFF="Power off"
+REBOOT="Reboot"
+SUSPEND="Suspend"
+HIBERNATE="Hibernate"
+LOGOUT="Logout"
+
+
+if [ x"$@" = x"${POWEROFF}" ]; then
+    systemctl poweroff
+    exit 0
+
+elif [ x"$@" = x"${REBOOT}" ]; then
+    systemctl reboot
+    exit 0
+
+elif [ x"$@" = x"${SUSPEND}" ]; then
+    systemctl suspend
+    exit 0
+
+elif [ x"$@" = x"${HIBERNATE}" ]; then
+    systemctl hibernate
+    exit 0
+
+elif [ x"$@" = x"${LOGOUT}" ]; then
+    i3-msg exit
+    exit 0
+fi
+
+
+echo $POWEROFF
+echo $REBOOT
+echo $SUSPEND
+echo $HIBERNATE
+echo $LOGOUT
